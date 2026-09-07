f3bfacef659aa0e9660a02b703a3621126aad0ca
mehdiougadi <ougadicloud@gmail.com>
2026-09-07 15:45:11 -0400

    Mehdi: added linting and formating

---
+++ utils/resource_creator.py
@@ -0,0 +1,25 @@
+import boto3
+
+
+def create_vpc():
+    pass
+
+
+def create_subnet():
+    pass
+
+
+def create_security_group():
+    pass
+
+
+def create_routing_Table():
+    pass
+
+
+def create_internet_gateway():
+    pass
+
+
+def create_ec2_instance():
+    pass
