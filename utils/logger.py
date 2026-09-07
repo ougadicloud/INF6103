15dcae736ba0760dc02af68157c39713f1956af0
mehdiougadi <ougadicloud@gmail.com>
2026-09-07 16:13:43 -0400

    Mehdi: implemented vpc and subnet creation

--- utils/logger.py
+++ utils/logger.py
@@ -3,29 +3,29 @@ import logging
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    
+
     if not logger.handlers:
         logger.setLevel(logging.INFO)
         handler = logging.StreamHandler()
         formatter = logging.Formatter("[%(levelname)s] %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
-    
+
     return logger
 
 
 class Logger:
     def __init__(self, name):
         self.logger = get_logger(name)
-    
+
     def info(self, msg):
         self.logger.info(msg)
-    
+
     def error(self, msg):
         self.logger.error(msg)
-    
+
     def warning(self, msg):
         self.logger.warning(msg)
-    
+
     def debug(self, msg):
         self.logger.debug(msg)
--- utils/resource_creator.py
+++ utils/resource_creator.py
@@ -1,12 +1,51 @@
 import boto3
+from botocore.exceptions import ClientError
+from logger import Logger
+
+log = Logger(__name__)
 
 
-def create_vpc():
-    pass
+def create_vpc(region="ca-central-1", cidr_block="10.0.0.0/16", name="default-vpc"):
+    log.info(f"Creating VPC: {name} ({cidr_block})")
+
+    try:
+        ec2 = boto3.client("ec2", region_name=region)
+        response = ec2.create_vpc(CidrBlock=cidr_block)
+        vpc_id = response["Vpc"]["VpcId"]
+
+        ec2.create_tags(Resources=[vpc_id], Tags=[{"Key": "Name", "Value": name}])
+
+        log.info(f"VPC created: {vpc_id}")
+        return vpc_id
+
+    except ClientError as e:
+        log.error(f"Failed to create VPC: {e}")
+        return None
 
 
-def create_subnet():
-    pass
+def create_subnet(
+    vpc_id,
+    region="ca-central-1",
+    cidr_block="10.0.1.0/24",
+    availability_zone="ca-central-1a",
+    name="default-subnet",
+    is_public=True,
+):
+    log.info(f"Creating subnet: {name} ({cidr_block}) - Public: {is_public}")
+
+    try:
+        ec2 = boto3.client("ec2", region_name=region)
+        response = ec2.create_subnet(VpcId=vpc_id, CidrBlock=cidr_block, AvailabilityZone=availability_zone)
+        subnet_id = response["Subnet"]["SubnetId"]
+
+        ec2.create_tags(Resources=[subnet_id], Tags=[{"Key": "Name", "Value": name}])
+
+        log.info(f"Subnet created: {subnet_id}")
+        return subnet_id
+
+    except ClientError as e:
+        log.error(f"Failed to create subnet: {e}")
+        return None
 
 
 def create_security_group():
